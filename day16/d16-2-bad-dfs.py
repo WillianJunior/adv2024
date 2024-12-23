bdc1411fded815d825bd06bc48ac4354afea78f5
Willian <guns945@gmail.com>
2024-12-23 12:38:14 -0300

    just part1 again...

---
+++ day16/d16-2-bad-dfs.py
@@ -0,0 +1,373 @@
+# --- Day 16: Reindeer Maze ---
+
+# It's time again for the Reindeer Olympics! This year, the big
+# event is the Reindeer Maze, where the Reindeer compete for the
+# loright score.
+
+# You and The Historians arrive to search for the Chief right as
+# the event is about to start. It wouldn't hurt to watch a little,
+# right?
+
+# The Reindeer start on the Start Tile (marked S) facing East and
+# need to reach the End Tile (marked E). They can move forward one
+# tile at a time (increasing their score by 1 point), but never
+# into a wall (#). They can also rotate clockwise or counterclockwise
+#     90 degrees at a time (increasing their score by 1000 points).
+
+# To figure out the best place to sit, you start by grabbing a map
+# (your puzzle input) from a nearby kiosk. For example:
+
+# ###############
+# #.......#....E#
+# #.#.###.#.###.#
+# #.....#.#...#.#
+# #.###.#####.#.#
+# #.#.#.......#.#
+# #.#.#####.###.#
+# #...........#.#
+# ###.#.#####.#.#
+# #...#.....#.#.#
+# #.#.#.###.#.#.#
+# #.....#...#.#.#
+# #.###.#.#.#.#.#
+# #S..#.....#...#
+# ###############
+
+# There are many paths through this maze, but taking any of the best
+# paths would incur a score of only 7036. This can be achieved by taking
+# a total of 36 steps forward and turning 90 degrees a total of 7 times:
+
+# ###############
+# #.......#....E#
+# #.#.###.#.###^#
+# #.....#.#...#^#
+# #.###.#####.#^#
+# #.#.#.......#^#
+# #.#.#####.###^#
+# #..>>>>>>>>v#^#
+# ###^#.#####v#^#
+# #>>^#.....#v#^#
+# #^#.#.###.#v#^#
+# #^....#...#v#^#
+# #^###.#.#.#v#^#
+# #S..#.....#>>^#
+# ###############
+
+# Here's a second example:
+
+# #################
+# #...#...#...#..E#
+# #.#.#.#.#.#.#.#.#
+# #.#.#.#...#...#.#
+# #.#.#.#.###.#.#.#
+# #...#.#.#.....#.#
+# #.#.#.#.#.#####.#
+# #.#...#.#.#.....#
+# #.#.#####.#.###.#
+# #.#.#.......#...#
+# #.#.###.#####.###
+# #.#.#...#.....#.#
+# #.#.#.#####.###.#
+# #.#.#.........#.#
+# #.#.#.#########.#
+# #S#.............#
+# #################
+
+# In this maze, the best paths cost 11048 points; following one such
+# path would look like this:
+
+# #################
+# #...#...#...#..E#
+# #.#.#.#.#.#.#.#^#
+# #.#.#.#...#...#^#
+# #.#.#.#.###.#.#^#
+# #>>v#.#.#.....#^#
+# #^#v#.#.#.#####^#
+# #^#v..#.#.#>>>>^#
+# #^#v#####.#^###.#
+# #^#v#..>>>>^#...#
+# #^#v###^#####.###
+# #^#v#>>^#.....#.#
+# #^#v#^#####.###.#
+# #^#v#^........#.#
+# #^#v#^#########.#
+# #S#>>^..........#
+# #################
+
+# Note that the path shown above includes one 90 degree turn as the very
+# first move, rotating the Reindeer from facing East to facing North.
+
+# Analyze your map carefully. What is the loright score a Reindeer
+# could possibly get?
+
+import numpy as np
+from tqdm import tqdm
+from multiprocessing import Pool
+from time import time
+from numba import jit
+from collections import defaultdict
+from math import ceil, floor, prod
+import sys
+
+import numpy as np
+from tqdm import tqdm
+from multiprocessing import Pool
+from time import time
+from numba import jit
+from collections import defaultdict
+from math import ceil, floor, prod
+import sys
+from heapq import heapify, heappush, heappop
+
+np.set_printoptions(threshold=np.inf)
+np.set_printoptions(linewidth=np.inf)
+
+
+def print_mat(m):
+    np.set_printoptions(threshold=np.inf)
+    np.set_printoptions(linewidth=np.inf)
+    print(np.array2string(m, separator='', formatter={'str_kind':
+                                                      lambda x: x}))
+
+
+def conv_2d(arr, conv_f):
+    return [[conv_f(a) for a in line] for line in arr]
+
+
+def flatten(xss):
+    return [x for xs in xss for x in xs]
+
+
+def get_neighb(pos, shape):
+    h, w = shape
+    i, j = pos
+
+    ns = []
+    if i > 0:
+        ns.append((i - 1, j))
+    if i < h - 1:
+        ns.append((i + 1, j))
+    if j > 0:
+        ns.append((i, j - 1))
+    if j < w - 1:
+        ns.append((i, j + 1))
+
+    return ns
+
+
+left = '<'
+right = '>'
+down = 'v'
+up = '^'
+turn_cost = 1000
+
+
+def get_dir(p1, p2):
+    # From p1->p2
+    if p1[0] == p2[0]:
+        if p1[1] > p2[1]:
+            return left
+        else:
+            return right
+    else:
+        if p1[0] > p2[0]:
+            return up
+        else:
+            return down
+
+
+def is_reverse_dir(d1, d2):
+    if (d1 == up and d2 == down) or (d2 == up and d1 == down):
+        return True
+    if (d1 == left and d2 == right) or (d2 == left and d1 == right):
+        return True
+    return False
+
+
+def get_turn_cost(prev_pos, cur_pos, next_pos):
+    d1 = get_dir(prev_pos, cur_pos)
+    d2 = get_dir(cur_pos, next_pos)
+    if d1 == d2:
+        return 1
+    if is_reverse_dir(d1, d2):
+        return 2 * turn_cost + 1
+    return turn_cost + 1
+
+
+def dktr2D(visited_dist, start_pos, end_pos=None, blockage=-1):
+    # dikstra all paths algorithm for a 2D mat 'visited_dist'
+    # 'visited_dist' have its path blocked by a blocking value
+    # which cannot be traversed.
+    # It is assumed that 'visited_dist' have all unvisited values
+    # as max_value and its start position is 0.
+    # If end_pos is not none, it will stop when reached
+
+    # This means: start pos direction is right
+    prev_start_pos = (start_pos[0], start_pos[1] - 1)
+
+    shape = visited_dist.shape
+    unvisited_queue = []
+    heappush(unvisited_queue,
+             (visited_dist[start_pos], prev_start_pos, start_pos))
+
+    # Try to find paths while there are options
+    while len(unvisited_queue) > 0:
+        dist, prev_pos, cur_pos = heappop(unvisited_queue)
+
+        if cur_pos == end_pos:
+            continue
+
+        ns = get_neighb(cur_pos, shape)
+        for next_pos in ns:
+            new_dist = dist + get_turn_cost(prev_pos, cur_pos, next_pos)
+            if visited_dist[next_pos] != blockage and new_dist < visited_dist[
+                    next_pos]:
+                visited_dist[next_pos] = new_dist
+                heappush(unvisited_queue, (new_dist, cur_pos, next_pos))
+
+
+all_paths = []
+
+
+def graph2D_dfs(maze,
+                cur_path,
+                cur_cost,
+                prev_pos,
+                cur_pos,
+                end_pos,
+                blockage=-1,
+                level=0):
+
+    shape = maze.shape
+
+    print(f'search {cur_pos} l{level} size{len(cur_path)}')
+
+    ns = get_neighb(cur_pos, shape)
+    for next_pos in ns:
+        next_path = cur_path + [next_pos]
+        next_cost = cur_cost + get_turn_cost(prev_pos, cur_pos, next_pos)
+
+        # Skip walls
+        if maze[next_pos] == blockage:
+            continue
+
+        # Found a path
+        if next_pos == end_pos:
+            all_paths.append((next_cost, next_path))
+            # print(f'found path {next_cost, next_path}')
+            # print(set_path_base(maze, next_path))
+            # input()
+            break
+
+        # No need to go backwards
+        if is_reverse_dir(get_dir(prev_pos, cur_pos),
+                          get_dir(cur_pos, next_pos)):
+            continue
+
+        # No cycles
+        if next_pos in cur_path:
+            continue
+
+        graph2D_dfs(maze, next_path, next_cost, cur_pos, next_pos, end_pos,
+                    blockage, level+1)
+
+
+def find_path_from_visited(visited_dist, start_pos, end_pos, blockage=-1):
+    pos = end_pos
+    path = [end_pos]
+    shape = visited_dist.shape
+    while pos != start_pos:
+        best_dist = sys.maxsize
+        best_next = None
+        for n in get_neighb(pos, shape):
+            if visited_dist[n] != blockage and visited_dist[n] < best_dist:
+                best_dist = visited_dist[n]
+                best_next = n
+
+        if best_next == None:
+            return []
+        else:
+            path.append(best_next)
+            pos = best_next
+
+    return path
+
+
+def set_path(m, path, path_val=99):
+    m2 = m.copy()
+    h, w = m.shape
+    for p in path:
+        m2[p] = path_val
+    for i in range(h):
+        for j in range(w):
+            if m2[i, j] > sys.maxsize / 100:
+                m2[i, j] = -10
+    return m2
+
+def set_path_base(m, path, path_val='O'):
+    m2 = m.copy()
+    h, w = m.shape
+    for p in path:
+        m2[p] = path_val
+    # for i in range(h):
+    #     for j in range(w):
+    #         if m2[i, j] > sys.maxsize / 100:
+    #             m2[i, j] = -10
+    return m2
+
+
+def main():
+    inpt = []
+    with open('input.txt', 'r') as f_in:
+        inpt = f_in.readlines()
+
+        # Remove \n
+        inpt = [i[:-1] for i in inpt]
+
+    start_char = 'S'
+    end_char = 'E'
+    path_char = '.'
+    wall_char = '#'
+
+    max_cost = sys.maxsize
+    wall = -1
+
+    maze = np.array([list(i) for i in inpt])
+    h, w = maze.shape
+
+    search_space = np.full((h, w), wall)
+
+    for i in range(h):
+        for j in range(w):
+            if maze[i, j] == path_char:
+                # print(maze[i,j])
+                search_space[i, j] = max_cost
+            elif maze[i, j] == start_char:
+                start_pos = i, j
+                search_space[start_pos] = 0
+            elif maze[i, j] == end_char:
+                end_pos = i, j
+                search_space[i, j] = max_cost
+
+    # This means: start pos direction is right
+    sys.setrecursionlimit(4000)
+    prev_start_pos = (start_pos[0], start_pos[1] - 1)
+    graph2D_dfs(maze, [], 0, prev_start_pos, start_pos, end_pos, wall_char)
+
+    print(all_paths)
+
+    best_cost = min([a for a,b in all_paths])
+
+    for cost, path in all_paths:
+        if cost == best_cost:
+            for p in path:
+                maze[p] = 'O'
+
+    print(maze)
+    print(sum(sum(maze=='O')))
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ day23/d23-1.py
@@ -0,0 +1,134 @@
+# --- Day 23: LAN Party ---
+
+# As The Historians wander around a secure area at Easter Bunny
+# HQ, you come across posters for a LAN party scheduled for today!
+# Maybe you can find it; you connect to a nearby datalink port and
+# download a map of the local network (your puzzle input).
+
+# The network map provides a list of every connection between two
+# computers. For example:
+
+# kh-tc
+# qp-kh
+# de-cg
+# ka-co
+# yn-aq
+# qp-ub
+# cg-tb
+# vc-aq
+# tb-ka
+# wh-tc
+# yn-cg
+# kh-ub
+# ta-co
+# de-co
+# tc-td
+# tb-wq
+# wh-td
+# ta-ka
+# td-qp
+# aq-cg
+# wq-ub
+# ub-vc
+# de-ta
+# wq-aq
+# wq-vc
+# wh-yn
+# ka-de
+# kh-ta
+# co-tc
+# wh-qp
+# tb-vc
+# td-yn
+
+# Each line of text in the network map represents a single
+# connection; the line kh-tc represents a connection between the
+# computer named kh and the computer named tc. Connections aren't
+# directional; tc-kh would mean exactly the same thing.
+
+# LAN parties typically involve multiplayer games, so maybe you can
+# locate it by finding groups of connected computers. Start by
+# looking for sets of three computers where each computer in the
+# set is connected to the other two computers.
+
+# In this example, there are 12 such sets of three inter-connected
+# computers:
+
+# aq,cg,yn
+# aq,vc,wq
+# co,de,ka
+# co,de,ta
+# co,ka,ta
+# de,ka,ta
+# kh,qp,ub
+# qp,td,wh
+# tb,vc,wq
+# tc,td,wh
+# td,wh,yn
+# ub,vc,wq
+
+# If the Chief Historian is here, and he's at the LAN party, it
+# would be best to know that right away. You're pretty sure his
+# computer's name starts with t, so consider only sets of three
+# computers where at least one computer's name starts with t. That
+# narrows the list down to 7 sets of three inter-connected computers:
+
+# co,de,ta
+# co,ka,ta
+# de,ka,ta
+# qp,td,wh
+# tb,vc,wq
+# tc,td,wh
+# td,wh,yn
+
+# Find all the sets of three inter-connected computers. How many
+# contain at least one computer with a name that starts with t?
+
+import numpy as np
+from tqdm import tqdm
+from multiprocessing import Pool
+from time import time
+from numba import jit
+from collections import defaultdict
+from math import ceil, floor, prod
+import sys
+from heapq import heapify, heappush, heappop
+
+np.set_printoptions(threshold=np.inf)
+np.set_printoptions(linewidth=np.inf)
+
+
+def main():
+    inpt = []
+    with open('input.txt', 'r') as f_in:
+        inpt = f_in.readlines()
+
+        # Remove \n
+        inpt = [i[:-1] for i in inpt]
+
+    edges = defaultdict(lambda: [])
+    hist_edges = defaultdict(lambda: [])
+
+    for line in inpt:
+        pc1, pc2 = line.split('-')
+        edges[pc1].append(pc2)
+        edges[pc2].append(pc1)
+
+        if pc1[0] == 't':
+            hist_edges[pc1].append(pc2)
+        if pc2[0] == 't':
+            hist_edges[pc2].append(pc1)
+
+    tree_ways = set()
+    for hist, neigh in hist_edges.items():
+        for i, pc1 in enumerate(neigh):
+            for pc2 in neigh[i + 1:]:
+                if pc1 in edges[pc2]:
+                    tree_ways.add(tuple(sorted([hist, pc1, pc2])))
+
+    # print(sorted(tree_ways))
+    print(len(sorted(tree_ways)))
+
+
+if __name__ == '__main__':
+    main()
